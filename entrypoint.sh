7aa862412eef7121d8191f961ba00293499d860e
🦀 adia-dev <abdoudu78130@gmail.com>
2024-04-18 23:09:41 +0200

    WIP: setup_slapd script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Use 'set -e' to exit the script when a command fails
+set -e
+
+# Use 'trap' to capture EXIT signals and handle errors
+trap 'error_handler' ERR
+
+# Function to handle errors
+error_handler() {
+    echo "[ERROR] An error occurred during LDAP setup." >&2
+    # Here you can add additional logging or error notification mechanisms
+    exit 1
+}
+
+# Set up OpenLDAP services
 setup_openldap() {
     echo "Setting up OpenLDAP services..."
     /setup-openldap/scripts/setup_slapd.sh
@@ -35,7 +49,11 @@ main() {
     echo "System is ready."
 }
 
+# Echo the initialization message
 echo "Initialization sequence is now backgrounded."
+
+# Run the main function in the background
 (sleep 3; main) &
 
+# Execute /usr/sbin/init in the foreground
 exec /usr/sbin/init
--- setup-openldap/scripts/setup_slapd.sh
+++ setup-openldap/scripts/setup_slapd.sh
@@ -1,37 +1,87 @@
 #!/bin/bash
 
-set -e
+set -Eeuo pipefail
 
-echo "Attempting to start slapd service..."
-if systemctl enable slapd && systemctl start slapd; then
-    echo "slapd started successfully."
-else
-    echo "Error: Failed to setup slapd."
+log() {
+    echo "[setup_slapd] $1" | tee -a /var/log/openldap_setup.log
+}
+
+handle_error() {
+    log "ERROR: An error occurred in the LDAP setup process."
     exit 1
-fi
+}
 
-echo "Copying DB_CONFIG to /var/lib/ldap..."
-cp /setup-openldap/database/DB_CONFIG /var/lib/ldap/DB_CONFIG
+ldap_modify() {
+    echo "[setup_slapd] Applying modification: $1" | tee -a /var/log/openldap_setup.log
+    ldapmodify -Q -Y EXTERNAL -H ldapi:/// -f $1
+}
 
-echo "Changing ownership of /var/lib/ldap and /setup-openldap to ldap:ldap..."
-chown -R ldap:ldap /var/lib/ldap/
-chown -R ldap:ldap /setup-openldap
+ldap_add() {
+    echo "[setup_slapd] Adding: $1" | tee -a /var/log/openldap_setup.log
+    ldapadd -Q -Y EXTERNAL -H ldapi:/// -f $1
+}
 
-echo "Adding LDAP core schemas..."
-ldapadd -Q -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/cosine.ldif
-ldapadd -Q -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/nis.ldif
-ldapadd -Q -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif
+ldap_add_binded() {
+    echo "[setup_slapd] Adding: $1" | tee -a /var/log/openldap_setup.log
+    LDAP_PASSWORD='Respons11'
+    ldapadd -xc -w $LDAP_PASSWORD -D 'cn=admin,dc=example,dc=com' -f $1
+}
 
-echo "Adding LDAP custom schemas..."
-ldapadd -Q -Y EXTERNAL -H ldapi:/// -f /setup-openldap/config/schema/criteo.ldif
+trap 'handle_error' ERR
 
-echo "Setting up logging"
-ldapmodify -Q -Y EXTERNAL -H ldapi:/// -f /setup-openldap/ldif/core/logging.ldif
+start_slapd_service() {
+    log "Attempting to start slapd service..."
+    systemctl enable slapd && systemctl start slapd
+    log "slapd started successfully."
+}
 
-echo "Setting up db indexes"
-ldapmodify -Q -Y EXTERNAL -H ldapi:/// -f /setup-openldap/ldif/core/db_index.ldif
+prepare_ldap_environment() {
+    log "Copying DB_CONFIG to /var/lib/ldap..."
+    cp /setup-openldap/database/DB_CONFIG /var/lib/ldap/DB_CONFIG
 
-echo "Restarting slapd to apply changes..."
-systemctl restart slapd
+    log "Changing ownership of /var/lib/ldap and /setup-openldap to ldap:ldap..."
+    chown -R ldap:ldap /var/lib/ldap/ /setup-openldap/
+}
 
-echo "slapd is running and ready to use."
+add_ldap_schemas() {
+    log "Adding LDAP core schemas..."
+    ldap_add /etc/openldap/schema/cosine.ldif
+    ldap_add /etc/openldap/schema/nis.ldif
+    ldap_add /etc/openldap/schema/inetorgperson.ldif
+
+    log "Adding LDAP custom schemas..."
+    ldap_add /setup-openldap/config/schema/criteo.ldif
+}
+
+update_ldap_config() {
+    log "Setting up logging"
+    ldap_modify /setup-openldap/ldif/logging.ldif
+
+    # log "Setting up db indexes"
+    # ldap_modify -Q -Y EXTERNAL -H ldapi:/// -f /setup-openldap/ldif/db_index.ldif
+
+    log "Setting up root pw..."
+    ldap_modify /setup-openldap/ldif/ch_root_pw.ldif
+
+    log "Setting up domain..."
+    ldap_modify /setup-openldap/ldif/ch_domain.ldif
+
+    log "Setting up organization..."
+    ldap_add_binded /setup-openldap/ldif/organization.ldif
+}
+
+restart_slapd() {
+    log "Restarting slapd to apply changes..."
+    systemctl restart slapd
+    log "slapd is running and ready to use."
+}
+
+main() {
+    start_slapd_service
+    prepare_ldap_environment
+    add_ldap_schemas
+    update_ldap_config
+    restart_slapd
+}
+
+main
